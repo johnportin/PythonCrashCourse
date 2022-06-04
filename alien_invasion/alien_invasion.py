7d15f3c25a1a185feeb99024e26b95d1e7bbfbde
johnportin <jportin13@gmail.com>
2022-06-04 01:08:20 -0500

    Change something... I forgot

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -36,7 +36,6 @@ class AlienInvasion:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
-            # print(len(self.bullets))
             self._update_screen()
             
             
